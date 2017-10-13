f297882fea3481e02f94c7ea0a0e055ffedfb2f6
nurrahman1995 <nurrahman1995@gmail.com>
2017-10-13 19:24:37 +0700

    first commit

---
+++ app/src/main/java/com/nurrahman/tugasdua/Npm.java
@@ -0,0 +1,15 @@
+package com.nurrahman.tugasdua;
+
+import android.content.res.Resources;
+
+/**
+ * Created by Run on 11/10/2017.
+ */
+
+class Npm {
+    private static Resources text;
+
+    public static Resources getText() {
+        return text;
+    }
+}
